67bdc553e8d402e41ac2d5bfcc6aa6bfa557aef0
Muhammad Umair <umairabid.uc@gmail.com>
2024-07-09 13:35:10 +0500

    Added 'Add Topic' functionality

--- main.js
+++ main.js
@@ -1,50 +1,51 @@
 //lert("Script is working!!")
 
-let form = document.getElementById("form");
-function handleForm(event) { 
-    event.preventDefault(); 
-    formData.topic = document.getElementById("topic").innerText;
-    console.log(document.getElementById("form")[0].innerText);
-    formData.hours = document.getElementById("hours").innerText;
-    formData.mins = document.getElementById("mins").innerText;
-    formData.link = document.getElementById("link").innerText;
-    console.log(formData)
-} 
-form.addEventListener('submit', handleForm);
 
-let overlay = document.getElementById("overlay")
-let bg = document.getElementsByTagName("table")
-console.log(overlay)
-let addTopic = document.getElementById("btn-add")
-addTopic.addEventListener("click", ()=>{
+function toggleOverlay() {
+
+    let overlay = document.getElementById("overlay")
+    let bg = document.getElementsByTagName("table")
     overlay.style.display = (overlay.style.display === "flex") ? "none" : "flex";
     bg[0].style.filter = (bg[0].style.filter === "blur(6px)") ? "none" : "blur(6px)";
-})
+
+}
+console.log(overlay)
+let addTopic = document.getElementById("btn-add")
+addTopic.addEventListener("click", toggleOverlay)
 
 let btnShowHide = document.querySelectorAll(".showHide")
 
 btnShowHide.forEach(btn => {
-    btn.addEventListener("click", ()=> {
+    btn.addEventListener("click", () => {
         console.log("Clicked")
     })
 })
 
 
 
-let formData = {
-    topic: "",
-    hours: "",
-    mins: "",
-    link: ""
+let form = document.querySelector("#form");
+function handleForm(event) {
+    event.preventDefault();
+    let fd = new FormData(form)
+    console.log(fd)
+    console.log("_________")
+    console.log(fd.get("topic"))
+    for (const data of fd) {
+        console.log(data)
+    }
+    let table = document.getElementById("table-body")
+    table.innerHTML += `<tr>
+                <td><input type="checkbox" class="row-ckbx" /></td>
+                <td>${fd.get("topic")}</td>
+                <td>${fd.get("hours")} Hours ${fd.get("mins")} Minutes</td>
+                <td><a href="${fd.get("link")}"> Link </td>
+                <td><button class="showHide" id="sh">Show/Hide</button></td>
+            </tr>`;
+    toggleOverlay()
+
 }
+form.addEventListener('submit', handleForm);
 
-let submitBtn = document.getElementById("btn-add-topic")
 
-submitBtn.addEventListener("submit", ()=> {
-    formData.topic = document.getElementById("topic").innerText;
-    formData.hours = document.getElementById("hours").innerText;
-    formData.mins = document.getElementById("mins").innerText;
-    formData.link = document.getElementById("link").innerText;
-    console.log(formData)
-})
+
 
